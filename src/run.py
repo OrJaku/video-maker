7acde1867f63689f808ad8e261264a5abe63241b
OrJaku <kuba.orzel@hotmail.com>
2020-04-19 20:11:44 +0200

    Open file from sys path

--- src/run.py
+++ src/run.py
@@ -1,30 +1,58 @@
 from PyQt5 import QtWidgets, uic
-from video_app import Main, LoadVideo
+from PyQt5.QtCore import QDir
+from video_app import Main, LoadVideo, basedir
+
+app = QtWidgets.QApplication([])
+dlg = uic.loadUi('run.ui')
 
 
-def loaded_video_data():
-    load_file = LoadVideo()
+def loaded_video_data(path):
+    load_file = LoadVideo(path)
     dlg.fpsVideo.setText(str(load_file.cap_video_fps))
     dlg.lengthVideo.setText(str(load_file.video_length))
     dlg.widthVideo.setText(str(load_file.cap_video_width))
     dlg.heightVideo.setText(str(load_file.cap_video_height))
 
+    dlg.fpsConvert.setText(str(""))
+
+    dlg.fpsConvert.setText('1')
+    dlg.widthConvert.setText(str(load_file.cap_video_width))
+    dlg.heightConvert.setText(str(load_file.cap_video_height))
+    dlg.lengthConvert.setText(str(load_file.video_length))
+
+    dlg.ComlitedLbl.setText(str(""))
+
 
 def converting():
+    path_to_file = dlg.filePathWindow.text()
     fps = dlg.fpsConvert.text()
     width = dlg.widthConvert.text()
     height = dlg.heightConvert.text()
-    color = dlg.colorConvert.text()
-    main = Main(fps, color, width, height)
+    if dlg.colorRadio.isChecked():
+        color = 1
+    else:
+        color = 0
+    main = Main(fps, color, width, height, path_to_file)
     main.modify()
+    dlg.ComlitedLbl.setText(str("Completed"))
 
 
-app = QtWidgets.QApplication([])
-dlg = uic.loadUi('run.ui')
+def get_file():
+    file_name = QtWidgets.QFileDialog.getOpenFileName(None, 'Open file', QDir.homePath())[0]
+    dlg.filePathWindow.setText(file_name)
+    loaded_video_data(file_name)
 
-dlg.loadVideo.clicked.connect(loaded_video_data)
+
+dlg.fpsVideo.setReadOnly(True)
+dlg.lengthVideo.setReadOnly(True)
+dlg.widthVideo.setReadOnly(True)
+dlg.heightVideo.setReadOnly(True)
+dlg.ComlitedLbl.setReadOnly(True)
+
+dlg.browseButton.clicked.connect(get_file)
 dlg.convertVideo.clicked.connect(converting)
 
-dlg.show()
+if __name__ == "__main__":
 
-app.exec()
+    dlg.show()
+    app.exec()
--- src/video_app.py
+++ src/video_app.py
@@ -1,12 +1,14 @@
 import os
 import cv2
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 class LoadVideo:
-    def __init__(self):
-        self.basedir = os.path.abspath(os.path.dirname(__file__))
-        self.input_path = os.path.join(self.basedir, "input/hc.mp4")
-        self.output_path = os.path.join(self.basedir, "output/out_hc.avi")
+    def __init__(self, path_to_file):
+        self.path = path_to_file
+
+        self.input_path = os.path.join(self.path)
         self.video_file = cv2.VideoCapture(self.input_path)
 
         self.video_length = int(self.video_file.get(cv2.CAP_PROP_FRAME_COUNT))
@@ -16,18 +18,17 @@ class LoadVideo:
 
 
 class Main:
-    def __init__(self, fps, color, weight, height):
-
-        self.file = LoadVideo()
+    def __init__(self, fps, color, weight, height, path):
+        self.output_path = os.path.join(basedir, "output/out_hc.avi")
+        self.file = LoadVideo(path)
         self.video_width = int(weight)
         self.video_height = int(height)
 
         self.video_fps = (self.file.cap_video_fps * float(fps))
         self.video_color = int(color)
-        print("COLOR", type(self.video_color))
 
         self.fourcc = cv2.VideoWriter_fourcc(*'DIVX')
-        self.out = cv2.VideoWriter(self.file.output_path,
+        self.out = cv2.VideoWriter(self.output_path,
                                    self.fourcc,
                                    self.video_fps,
                                    (self.video_width, self.video_height),
@@ -35,12 +36,13 @@ class Main:
                                    )
 
     def modify(self):
-
         while True:
             ret, frame = self.file.video_file.read()
-            gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+            try:
+                gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+            except cv2.error:
+                break
             if ret:
-
                 if self.video_color == 0:
                     self.file.color_value = gray
                 elif self.video_color == 1:
@@ -56,7 +58,6 @@ class Main:
                 break
 
         self.out.release()
-        cv2.destroyAllWindows()
 
 
 
